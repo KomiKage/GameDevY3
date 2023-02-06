db917993f64750290c055aaf0209d4f993814f33
KomiKage <bryanvd654@gmail.com>
2023-02-06 14:22:13 +0100

    Added Enemy tag & JermaSphere

--- Assets/Scripts/Player/Projectile.cs
+++ Assets/Scripts/Player/Projectile.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     private bool collided;
+    private float lifetime = 3f;
     private void OnCollisionEnter(Collision co)
     {
         if(co.gameObject.tag != "Bullet" && co.gameObject.tag != "Player" && !collided)
         {
             collided = true;
+            Destroy(gameObject);
+        }
+
+        if(co.gameObject.tag == "Enemy")
+        {
+            Destroy(co.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+        {
             Destroy(gameObject);
         }
     }
