415401eccd5a16b1fb0df4dd99e0accbcd4ad92f
KomiKage <bryanvd654@gmail.com>
2023-02-06 11:53:30 +0100

    movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,25 +6,41 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    private float speed = 20f;
-    private float sprintMult = 1;
-    private float gravity = -9.7f;
-    private float jumpHeight = 7f;
+    private float speed = 12f;
+    private float gravity = -9.81f;
+    private float jumpHeight = 3f;
+    private float sprintMult = 1f;
 
-    private bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
     Vector3 velocity;
+    bool isGrounded;
 
-        private void Update()
+    private void Update()
     {
+        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        //isGrounded = controller.isGrounded;
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
-        isGrounded = controller.isGrounded;
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * sprintMult * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
@@ -35,11 +51,6 @@ public class PlayerMovement : MonoBehaviour
             sprintMult = 1f;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            Debug.Log("FUCK");
-            velocity.y += jumpHeight;
-        }
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
